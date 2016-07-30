8f7587db03d945d1b0adef3f3d878beccdbbdba0
ArcanuS <kitarofana@gmail.com>
2016-07-29 23:59:33 -0300

    entidad user implementando userInterface y serializable

--- form_login_auth_db/src/AppBundle/Entity/User.php
+++ form_login_auth_db/src/AppBundle/Entity/User.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
-class User
+class User implements UserInterface, \Serializable
 {
     /**
      * @var int
@@ -49,6 +50,11 @@ class User
      */
     private $isActive;
 
+    private function __construct()
+    {
+        $this->isActive = true;
+        $this->role = 'ROLE_USER';
+    }
 
     /**
      * Get id
@@ -155,5 +161,41 @@ class User
     {
         return $this->isActive;
     }
-}
 
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getRoles()
+    {
+        return array($this->getRole());
+    }
+
+    public function eraseCredentials()
+    {
+
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->role,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->role,
+        ) = unserialize($serialized);
+    }
+}
